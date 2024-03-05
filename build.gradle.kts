aee8c9821b7dab2e4d7894a1d1da4a618d72c70e
Mads Opheim <mads.opheim@nav.no>
2024-03-05 12:42:13 +0100

    Nytt forsøk

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ repositories {
     mavenCentral()
     mavenLocal()
     maven {
-        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
     }
 }
 
